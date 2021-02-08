f0ac193e8c073a350ac8009bb3f659f3f8887074
constantgillet <constant.gillet@hetic.net>
2021-02-08 11:21:21 +0100

    Add bottom bar icons

--- src/navigations/MainBottomNavigator.js
+++ src/navigations/MainBottomNavigator.js
@@ -3,14 +3,39 @@ import { View, Text } from 'react-native'
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
 import CategoriesScreen from '../screens/CategoriesScreen'
 import SearchScreen from '../screens/SearchScreen'
+import { MainStyle } from '../styles/styles'
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
 const TabNavigator = createBottomTabNavigator()
 
 export default function MainBottomNavigator() {
     return (
-        <TabNavigator.Navigator>
-            <TabNavigator.Screen name="Categories" component={CategoriesScreen}/>
-            <TabNavigator.Screen name="Search" component={SearchScreen}/>
+        <TabNavigator.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    backgroundColor: MainStyle.primaryColor
+                }
+            }}
+        >
+            <TabNavigator.Screen 
+                name="Categories" 
+                component={CategoriesScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="home" size={24} color="white" />
+                    ),
+                }}
+            />
+            <TabNavigator.Screen 
+                name="Search" 
+                component={SearchScreen}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <FontAwesome5 name="search" size={24} color="white" />
+                    ),
+                }}
+            />
         </TabNavigator.Navigator>
     )
 }
