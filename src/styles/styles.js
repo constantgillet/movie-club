fe82c93489400cf660615f778a31e4f6b9234ba2
constantgillet <constant.gillet@hetic.net>
2021-02-08 10:55:24 +0100

    Add style object

---
+++ src/styles/styles.js
@@ -0,0 +1,7 @@
+export const MainStyle = {
+    primaryColor: '#B00020',
+    secondaryColor: '#B5A90F',
+    darkColor: '#000000',
+    lightColor: '#FFFFFF',
+    screenBackgroundColor: '#F4F4F4'
+}
