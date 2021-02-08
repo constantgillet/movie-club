4d5f43e80f1679597865573f0a7c4e51e7248a6c
AntoineBaudot git config --global user.name "AntoineBaudot" git config --global user.name AntoineBaudot <baudotab@gmail.com>
2021-02-08 15:49:36 +0100

    add button categories and header

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,10 +1,34 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button, } from 'react-native'
+import ButtonCategories from '../components/ButtonCategories'
+import LogoHeader from '../components/LogoHeader'
+
 
 export default function CategoriesScreen() {
     return (
-        <View>
-            <Text>Ecran d'accueil</Text>
-        </View>
+        <view>
+            <View style={styles.headerLogo}>
+                <LogoHeader/>
+            
+            </View>
+                
+            <View style={styles.buttonCategories}>
+                <ButtonCategories/>
+            </View>
+        </view>
+
     )
 }
+
+
+const styles = StyleSheet.create({
+    headerLogo: {
+        flex: 1,
+        marginTop: 40
+    },
+    buttonCategories: {
+        flex: 2,
+        marginTop: 40
+    }
+})
+
