9b57d2115f6a947d63c32d330fcbb0a5c726168e
constantgillet <constant.gillet@hetic.net>
2021-02-08 09:56:51 +0100

    Add screens

---
+++ src/screens/MoviesListScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function MoviesListScreen() {
+    return (
+        <View>
+            <Text>Movies list</Text>
+        </View>
+    )
+}
